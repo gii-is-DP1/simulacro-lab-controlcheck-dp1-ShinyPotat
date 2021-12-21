d68cdbe53f613570e4ea6521b60f82c64089b1da
javpacmar <shinypotat@DESKTOP-U0S5O07.localdomain>
2021-12-21 11:44:10 +0100

    Primera solucion
    <body>
    
    <footer>

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,9 +2,16 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
-    public List<Product> getAllProducts(){
-        return null;
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
@@ -15,9 +22,8 @@ public class ProductService {
         return null;
     }
 
-    public Product save(Product p){
-        return null;       
+    public Product save(Product p) {
+        return null;
     }
 
-    
 }
